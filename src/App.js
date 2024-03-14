5ed4f04f7958a966b37f8687343e54951175d60e
Nithya Subramaniyan <nithya.subramaniyan@itp.objectfrontier.com>
2024-03-14 10:09:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import React from "react";
 
-function App() {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {Num1 : '', Num2 : '', Result : ''};
+  }
+
+  Add = () => {
+    var n1 = parseInt(this.state.Num1);
+    var n2 = parseInt(this.state.Num2);
+    var sum = n1 + n2;
+    this.setState( {Result : sum} );
+  }
+
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 data-testid="Heading"> Addition of Two Numbers </h1>
+      <br/>
+      <label data-testid="Label1"> Enter Number 1 : </label> <input data-testid="TextBox1" type="number" value={this.state.Num1} onChange={ (e) => {this.setState({Num1 : e.target.value});}}/>
+      <br/>
+      <label data-testid="Label2"> Enter Number 2 : </label> <input data-testid="TextBox2" type="number" value={this.state.Num2} onChange={ (e) => {this.setState({Num2 : e.target.value});}} />
+      <br/>
+      <input data-testid="AdditionButton" type="submit" value="Addition" onClick={this.Add}/>
+      <br/>
+      <b data-testid="Result">Sum : {this.state.Result} </b>
     </div>
   );
+  }
 }
 
 export default App;
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div>
+//       <h1 data-testid="MyHeading">Welcome to JEST Demo with React</h1>
+//       <br/>
+//       <b data-testid="MyContent">Hello..This is Nithu !</b>
+//     </div>
+//   );
+// }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,120 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('My Calculator Test Suite', () =>{
+
+  test('Test Case-1[Render All UI]',()=>{
+    render(<App/>);
+    const textBox1 = screen.getByTestId('TextBox1');
+    const textBox2 = screen.getByTestId('TextBox2');
+    const additionButton = screen.getByTestId('AdditionButton');
+
+    expect(textBox1).toBeInTheDocument();
+    expect(textBox2).toBeInTheDocument();
+    expect(additionButton).toBeInTheDocument();
+  });
+
+  test('Test Case-2[Render All Labels and Text Message]',()=> {
+    render(<App/>);
+    const heading = screen.getByTestId("Heading");
+    const label1 = screen.getByTestId("Label1");
+    const label2 = screen.getByTestId("Label2");
+    const result = screen.getByTestId("Result");
+
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent("Addition of Two Numbers");
+
+    expect(label1).toBeInTheDocument();
+    expect(label1).toHaveTextContent("Enter Number 1 :");
+
+    expect(label2).toBeInTheDocument();
+    expect(label2).toHaveTextContent("Enter Number 2 :");
+
+    expect(result).toBeInTheDocument();
+    expect(result).toHaveTextContent("Sum :");
+
+  });
+
+  test('Test Case-3 [Testing Texboxes Initial Value]',() =>{
+    render(<App/>);
+    const textBox1 = screen.getByTestId("TextBox1");
+    const textBox2 = screen.getByTestId("TextBox2");
+    expect(textBox1).toHaveTextContent('');
+    expect(textBox2).toHaveTextContent('');
+  });
+
+  test('Test Case-4 [Testing Empty or Blank Input]', () => {
+    render(<App/>);
+    const additionButton = screen.getByTestId("AdditionButton");
+
+    fireEvent.click(additionButton);
+
+    const result = screen.getByTestId("Result");
+    expect(result).toHaveTextContent("Sum : NaN");
+  });
+
+  test('Test Case-5 [Testing Success Result]', () => {
+    render(<App/>);
+    const textBox1 = screen.getByTestId("TextBox1");
+    const textBox2 = screen.getByTestId("TextBox2");
+    const additionButton = screen.getByTestId("AdditionButton");
+
+    fireEvent.change(textBox1, {target : {value : 10 } } );
+    fireEvent.change(textBox2, {target : {value : 20 } } );
+
+    fireEvent.click(additionButton);
+
+    const result = screen.getByTestId("Result");
+
+    expect(result).toHaveTextContent("Sum : 30");
+  });
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+
+
+
+
+// describe('My Test Suite', () =>{
+//   test('Test Case-1', () => {
+//     render(<App/>);
+//     //var text1 = screen.getByText('Welcome to JEST Demo with React');
+//     var element1 = screen.getByTestId("MyHeading");
+//     //expect(element1).toBeInTheDocument();
+//     expect(element1).toHaveTextContent('Welcome to JEST Demo with React');
+//   }) ;
+  
+//   it('Test Case-2', () =>{
+//     render(<App/>);
+//     const text2 = screen.getByText('Hello..This is Nithu !');
+//     expect(text2).toBeInTheDocument();
+//   })
+// });
